c9c4ce1545e7b45dc1ae0d946810989bfa78a5dc
Garreth Dottin <garreth.dottin@gmail.com>
2014-04-09 10:58:55 -0400

    tested out update_all for cron job

--- config/schedule.rb
+++ config/schedule.rb
@@ -1,5 +1,5 @@
 set :output, "/log/cron.log"
 
-every 1.day, at: "2:52 pm" do
+every 1.day, at: "3:03 pm" do
   rake "db:reset_pomodoro", :output => {:error => 'error.log', :standard => 'cron.log'}
 end
